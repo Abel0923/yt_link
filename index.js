5a92577d3b937e39abd1385d8880373d488b45e4
theabelw@gmail.com <theabelw@gmail.com>
2023-03-05 14:44:41 +0300

    add title and error handler

--- index.js
+++ index.js
@@ -16,24 +16,21 @@ app.use(jsonParser)
 
 app.get("/download", async (req, res) => {
 
-    console.log("BODY >>", req.body)
+    try {
+        let v_id = req.query.url.split('v=')[1]
+        const info = await ytdl.getInfo(v_id);
+        // require('fs').writeFileSync('infos.json', JSON.stringify(info))
+        let yt_info = info.formats.find(_format => {
+            if (_format.qualityLabel === "720p" && _format.audioBitrate !== null && _format.qualityLabel !== null) {
+                return _format
+            }
+        })
+        return res.send({ url: yt_info.url, title: info.videoDetails.title })
+    } catch (_err) {
+        return res.status(500).json("NO VIDEO FOUND!")
+    }
 
-    // let { url } = req.query.url
-    let v_id = req.query.url.split('v=')[1]
-    const info = await ytdl.getInfo(v_id);
-    // require('fs').writeFileSync('formats.json', JSON.stringify(info.formats))
-    let yt_info = info.formats.find(_format => {
-        if (_format.qualityLabel === "720p" && _format.audioBitrate !== null && _format.qualityLabel !== null) {
-            return _format
-        }
-    })
-    return res.send({ url: yt_info.url })
-    // const v_id = req.query.url.split('v=')[1];
 
-    // const info = await ytdl.getInfo(req.query.url);
-    // let links = info.formats.sort((a, b) => {
-    //     return a.mimeType < b.mimeType;
-    // })
 });
 
 
